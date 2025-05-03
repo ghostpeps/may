814546ed9d91a19fd7032887f84f2e430f24f717
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-03 08:38:18 -0700

    Update mayeleventh.py

--- mayeleventh.py
+++ mayeleventh.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+import time
+
 happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!", icon=":material/celebration:")
 if happy:
   st.balloons()
+  time.sleep(0.5)
+  st.balloons()
+st.image(image="Happy Mother's Day (4).png")
