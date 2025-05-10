99f115d0273a2df90bb11530ca982de505257db0
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-10 09:58:37 -0700

    Update mayeleventh.py

--- mayeleventh.py
+++ mayeleventh.py
@@ -4,6 +4,7 @@ import time
 
 happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!", icon=":material/celebration:")
 if happy:
+  st.audio("mother.wav", autoplay=True)
   st.balloons()
   time.sleep(0.5)
   st.balloons()
