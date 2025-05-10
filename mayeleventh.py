dec934b0611fd807c6106bd8b1c5a34c260664a2
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-10 14:01:08 -0700

    Update mayeleventh.py

--- mayeleventh.py
+++ mayeleventh.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import time
 
+st.title(":material/celebration: మాతృ దినోత్సవ శుభాకాంక్షలు!")
 happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!", icon=":material/celebration:")
 if happy:
   st.balloons()
