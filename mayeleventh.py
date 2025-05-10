b2f54f4dab042d8edbfa34fc61c9b8d1747e4c52
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-10 10:02:49 -0700

    Update mayeleventh.py

--- mayeleventh.py
+++ mayeleventh.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import time
 
-happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!",on_click=st.audio("mother.wav", autoplay=True) icon=":material/celebration:")
+happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!", on_click=st.audio("mother.wav", autoplay=True), icon=":material/celebration:")
 if happy:
   st.balloons()
   time.sleep(0.5)
