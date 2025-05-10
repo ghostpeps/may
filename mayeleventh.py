f0524da7f709a2d63586ba3503a3d29e0c62236d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-10 10:37:52 -0700

    Update mayeleventh.py

--- mayeleventh.py
+++ mayeleventh.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+import time
+
+happy = st.button(label="మాతృ దినోత్సవ శుభాకాంక్షలు!", icon=":material/celebration:")
+if happy:
+  st.balloons()
+  time.sleep(0.5)
+  st.balloons()
+st.write("-భవిష్")
+st.write("డాష్‌బోర్డ్‌లోని పెట్టెలో చూడండి")
